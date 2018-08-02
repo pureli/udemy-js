6b6eba60bd2be2c06e2f66e690c261199d63b5c6
黎映君 <j89828@gmail.com>
2018-08-03 01:46:42 +0800

    練習函式（帶參數）：定義任務、執行任務

--- js/all.js
+++ js/all.js
@@ -1,10 +1,23 @@
-var clothPrize = 1200;
-var pocketMoney = 500;
-var needMoney = clothPrize - pocketMoney;
+// 加總練習
+function count(oneNum,twoNum){
+    var total = oneNum + twoNum;
+    console.log('總價格為:'+total+'元');
+}
 
-document.getElementById('clothPrize').textContent = clothPrize;
-document.getElementById('pocketMoney').textContent = pocketMoney;
-document.getElementById('needMoney').textContent = clothPrize - pocketMoney;
+count(10,2);
 
-alert(needMoney);
+// 匯率換算：台幣轉日幣
+function calculator(ntdDollar){
+    var jpDollar = ntdDollar * 0.27;
+    console.log(jpDollar);
+}
 
+calculator(10000);
+
+// 菜市場花費加總
+function marketCost(apple,milk,cookie){
+    var total = apple*20 + milk*30 + cookie*20;
+    console.log(total);
+}
+
+marketCost(3,2,1);

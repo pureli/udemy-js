247d5aee71beddac8df10dccb7189cbc8861ff88
黎映君 <j89828@gmail.com>
2018-08-03 01:06:29 +0800

    變數練習 - 將一個標籤當作一個物件（賦予id)，並 get id 改變文字內容

--- js/all.js
+++ js/all.js
@@ -1,1 +1,10 @@
-document.getElementById("article3").textContent = "哈囉";
+var clothPrize = 1200;
+var pocketMoney = 500;
+var needMoney = clothPrize - pocketMoney;
+
+document.getElementById('clothPrize').textContent = clothPrize;
+document.getElementById('pocketMoney').textContent = pocketMoney;
+document.getElementById('needMoney').textContent = clothPrize - pocketMoney;
+
+alert(needMoney);
+

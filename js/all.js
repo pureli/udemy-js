e65e3296fd9f2c218dfbb0168d15d939f97b85ef
黎映君 <j89828@gmail.com>
2018-08-16 01:17:13 +0800

    練習 for ＋ if：找出專櫃業績達標

--- js/all.js
+++ js/all.js
@@ -1,17 +1,26 @@
-// for 練習：印出陣列中的屬性
-var classes = [{
-    name : 'Apple Class',
-    teacher : 'Sandy',
-    student : ['papa','momo','dodo']
-},
-{
-    name : 'Lemon Class',
-    teacher : 'Newyellow',
-    student : ['11','22','33']
-}];
+// for ＋ if 練習：鼓勵專櫃業績達標
+var skm = 
+[
+    {
+        name : '華哥爾內衣',
+        todaySale : 20000
+    },
+    {
+        name : '蕾戴絲內衣',
+        todaySale : 15000
+    },
+    {
+        name : '莎薇內衣',
+        todaySale : 5000
+    }
+]
 
-// 需求：印出兩個班級的名字
-// 初始狀態 ; 條件 ; 更新內容 
-for(var i=0;i<classes.length;i++){
-    console.log('第'+(i+1)+'個班級名字叫'+classes[i].name);
+// 檢查品牌銷售業績
+for( var i = 0; i < skm.length ; i++){
+    // console.log(skm[i].todaySale);
+    if(skm[i].todaySale >= 12000){
+        console.log(skm[i].name+'達標，做得好喔！');
+    }
 }
+
+

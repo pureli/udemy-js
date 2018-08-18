e78969e7c4e898b1ef54d763122da430519893a4
黎映君 <j89828@gmail.com>
2018-08-18 12:12:51 +0800

    練習 createElement 在 HTML 上增加節點

--- js/all.js
+++ js/all.js
@@ -1,65 +1,40 @@
-// js 塞 HTML 標籤
+// 利用 createElement 在 html 上增加節點
+// 相較於innereHTML的優點為：利用 js 把東西都組好後，再放到 html 上
 
-// 練習一、利用 js 在 html 中顯示文字＋樣式（textContent只是節點，不接受放樣式）
+// 練習一、
+// 第一步：利用createElement增加html上的元素
+var loveFood = document.createElement('em');
 
-// 第一種寫法
-// var dinner = document.getElementById('dinner');
-// dinner.innerHTML = '<h1 class="dinnerColor">鹹酥雞</h1>';
+// 第二步：利用js動態增加好內容
+loveFood.textContent = '蛋糕';
+// 或想增加樣式
+loveFood.setAttribute('class','foodColor');
 
-// 第二種寫法
-var dinner = document.getElementById('dinner');
-var dinnerColor = '<h1 class="dinnerColor">鹹酥雞</h1>';
-dinner.innerHTML = dinnerColor;
-
+// 第三步：使用appendChild將以上節點掛在想要顯示的地方（位置會出現在最後面）
+document.querySelector('.food').appendChild(loveFood);
 
 
-// 練習二、innerHTML 組字串的技巧
-// 第一種寫法
-// var loveVideo = document.querySelector('.video');
-// loveVideo.innerHTML = '<li><a href="http:www.google.com">Hello Catie</a></li>';
-// 第二種寫法
-var loveVideo = document.querySelector('.video');
-var link = 'http://www.yahoo.com.tw';
-var name = '查理';
-loveVideo.innerHTML = '<li><a href="'+link+'">我是'+name+'</a></li>';
-
-// 練習三、innerHTML 與 for
-// 從JSON資料依序撈出資料
-var farms = 
-[
+// 練習createElement 與 for 運用
+// 挑戰：列出農夫的名字
+var farms = [
     {
-        name: 'papaFarm',
+        name: 'MapleFarm',
         farmer: 'ArPu',
-        apple: 50
+        duck: 200
     },
     {
-        name: 'newFarm',
-        farmer: 'Newyellow',
-        apple: 20
+        name: 'PapaFarm',
+        farmer: 'Papa',
+        duck: 300
     }
 ]
-// 錯誤寫法
-for( var i=0; i<farms.length; i++ ){
-    var farmName = farms[i].name;
-    var farmerName = farms[i].farmer;
-    console.log(farmName+'農場的主人是'+farmerName);
-    // 練習顯示訊息在網頁上，by 動態塞值
-    var farmsAll = document.querySelector('.farmsInfo');
-    farmsAll.innerHTML = '<div>'+farmName+'農場的主人是'+farmerName+'</div>';
+
+// 解法：組好形式與內容後，賦予子節點秀在網頁上
+for( var i = 0; i < farms.length; i++){
+    // 如何呈現
+    var farmerList = document.createElement('li');
+    // 內容    
+    farmerList.textContent = farms[i].farmer;
+    // 秀出
+    document.querySelector('.farmerList').appendChild(farmerList);
 }
-// 正確寫法
-// **注意**因為 innerHTML 特性是後面資料會覆蓋掉前面，所以
-    // 1.變數要全域變數
-    // 2.需要一個空物件儲存每一筆撈出來的資料，再塞入 innerHTML
-
-    var str = '';
-
-    for( var i=0; i<farms.length; i++ ){
-    var farmName = farms[i].name;
-    var farmerName = farms[i].farmer;
-    var farmsAll = document.querySelector('.farmsInfo');
-    var content = '<div>'+farmName+'農場的主人是'+farmerName+'</div>';
-    str+=content;
-    }
-    farmsAll.innerHTML = str;
-

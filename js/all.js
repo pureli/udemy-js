241842285accbc2e7e1f543e4075b6dfeaea9452
黎映君 <j89828@gmail.com>
2018-08-20 16:04:45 +0800

    練習1.取消元素默認行為 2.了解元素所在位置 3.change-表單內容更動時觸發

--- js/all.js
+++ js/all.js
@@ -1,49 +1,60 @@
-// 練習ㄧ、按鈕觸發事件：有兩種寫法
-// 第一種寫法
-// 從此處開始
-// var button = document.querySelector('.btn');
+// 練習取消元素默認行為、了解元素所在位置
+// 此處開始
+// var menuEl = document.querySelector('.menu');
+// menuEl.addEventListener('click',function(e){
 
-//  button.onclick = function(){
-//      alert('hello');
-// }
-
-// 第二種寫法：事件監聽(選擇事件,匿名function,false)
-// 從此處開始
-// button.addEventListener('click',function(){alert('新寫法')},false);
-
-
-
-
-//練習二、利用 addEventListener 綁定按鈕兩個事件
-//**注意**：使用 addEventListener 的好處是可以綁定好幾個事件，而 onclick 只能一個
-
-// 綁定按鈕一：監聽一個事件
-// 從此處開始
-// var button1 = document.querySelector('.btn1');
-// button1.addEventListener('click',function(){alert('我是按鈕一')},false);
-
-// 綁定按鈕二：連續監聽兩個事件
-// 從此處開始
-// var button2 = document.querySelector('.btn2');
-// button2.addEventListener('click',function(){alert('我是按鈕二')},false);
-// button2.addEventListener('click',function(){alert('按鈕二再次出現')},false);
+//     //取消元素原本的默認行為
+//     //原本連結會跳轉到指定網頁
+//     //submit按鈕，先透過我的js查詢表單有沒有錯誤，post去傳送
+//     e.stopPropagation();
+//     e.preventDefault();
+    
+//     // e.target 了解目前元素所在位置
+//     console.log(e.target);
+// },
+//     'false');
 
 
 
-//練習三、說明 addEventListener 中，false、true 的差別
-// false --> 從指定元素往外層
-// true --> 從最外層找到指定元素
-// 舉例
-// 從此處開始
-// var boxBtn = document.querySelector('.box');
-// boxBtn.addEventListener('click',function()){alert('點到方塊囉');},false); 
 
-// var bodyBtn = document.querySelector('.bodyClass');
-// bodyBtn.addEventListener('click',function(){alert('你沒有點到方塊');},false);
+// 練習 change - 表單內容更動時觸發
+// 此處開始
+    var farms = 
+    [
+        {
+            name: 'Papa Farm',
+            farmer: 'ArPu',
+            disc: '文山區'
+        },
+        {
+            name: 'New Farm',
+            farmer: 'Newyellow',
+            disc: '大安區'
+        },
+        {
+            name: 'QQ Farm',
+            farmer: 'Qbebe',
+            disc: '大安區'
+        }
+    ]
 
-// **注意**如果要解決點到重複元素的問題，須另加一行語法
-var boxBtn = document.querySelector('.box');
-boxBtn.addEventListener('click',function(e){e.stopPropagation();alert('點到方塊囉');},false); 
+        // change 事件：選到該區秀出該區域的農場名字
+        var area = document.querySelector('.areaSelect');
+        var list = document.querySelector('.listFrom');
 
-var bodyBtn = document.querySelector('.bodyClass');
-bodyBtn.addEventListener('click',function(){alert('你沒有點到方塊');},false);
+        function changeFunc(e){
+            //get到區域名字
+            var areaName = e.target.value;
+            var str = '';
+            //當get到的區域名字等於陣列中的區域，秀出li字串
+            for( var i = 0; i < farms.length; i++){
+                if(areaName == farms[i].disc){  
+                    // // 組字串
+                    // finalPrint顯示在ul裡
+                    str += '<li>'+farms[i].farmer+'</li>';
+                }
+            }
+            list.innerHTML = str;
+        }
+
+        area.addEventListener('change',changeFunc, false);

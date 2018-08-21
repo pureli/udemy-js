79d42c2a55ac096add17b425f8ebadb0b0045983
黎映君 <j89828@gmail.com>
2018-08-21 16:46:57 +0800

    mouseover 練習：當滑鼠滑到指定內容時觸發觸發

--- js/all.js
+++ js/all.js
@@ -1,23 +1,39 @@
-// 練習：鍵盤點下，火箭上升
-var body = document.body;
+// 練習：mouseover - 當滑鼠滑到指定內容時觸發觸發
+var screenX = document.querySelector('.screenX');
+var screenY = document.querySelector('.screenY');
 
-function upFunc(e){
-    // 當按1時，火箭一往上升
-    // 以此類推
+var pageX = document.querySelector('.pageX');
+var pageY = document.querySelector('.pageY');
 
-    switch(e.keyCode){
-        case 49:
-            document.querySelector('.rocket1').style.bottom = '2000px';
-            break;
-        case 50:
-            document.querySelector('.rocket2').style.bottom = '2000px';
-            break;
-        case 51:
-            document.querySelector('.rocket3').style.bottom = '2000px';
-            break;        
-        
-    }
+var clientX = document.querySelector('.clientX');
+var clientY = document.querySelector('.clientY');
+
+ // 滑鼠圖片
+ var mouseImg = document.querySelector('.mouseImg');
+
+// 事件監聽
+
+function showFunc(e){
+    screenX.textContent = e.screenX;
+    screenY.textContent = e.screenY;
+    pageX.textContent = e.pageX;
+    pageY.textContent = e.pageY;
+    clientX.textContent = e.clientX;
+    clientY.textContent = e.clientY;
+    // 滑鼠圖片
+    mouseImg.style.left = e.clientX+'px';
+    mouseImg.style.top = e.clientY+'px';
+}
+
+var el= document.body;
+el.addEventListener('mousemove',showFunc,false);
+
+// 碰到怪物跳出訊息
+var box1 = document.querySelector('.box1');
+box1.addEventListener('mousemove',box,false);
+
+function box(){
+    alert('hello');
 }
 
 
-body.addEventListener('keydown',upFunc,false);

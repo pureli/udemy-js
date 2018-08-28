dcda537af07ebc83e49c736e26a0f9374ccff328
黎映君 <j89828@gmail.com>
2018-08-28 22:33:56 +0800

    練習透過 JSON.parse 和 JSON.stringify 編譯資料

--- js/all.js
+++ js/all.js
@@ -1,29 +1,23 @@
-// 練習 localStorage 存、取資料到瀏覽器資料庫
+// 透過 JSON.parse 和 JSON.stringify 編譯資料
 
-// var str = 'hello';
-// localStorage.setItem("myName",str);
-// console.log(localStorage.getItem('myName'));
+// 練習1.將 array 轉為 string
+// JSON.stringify()
 
-// 練習撈文字
-// 第一步：click按鈕存資料到 local storage
-var btn = document.querySelector('.btnClass');
+// 練習2.將 string 轉為 array
+// JSON.parse()
+// 學以上兩點是因為 localStorage 只會保存 string 資料
 
+// 此處開始
+var country = [{farmer: "Ema"}];
+// 第一步：先將陣 array 轉為 string
+var countryString = JSON.stringify(country);
+// 第二步：將資料存到 local storage
+localStorage.setItem('countryItem',countryString);
 
-btn.addEventListener('click',saveToStorage,false);
-function saveToStorage(){
-    // 存到 local storage
-    var text = document.querySelector('.textClass').value;
-    localStorage.setItem('myName',text);
-}
-// 第一步：新增一個按鈕，呼叫存到 local storage 裡的名字
-var btnCall = document.querySelector('.btnCall');
-btnCall.addEventListener('click',callFun,false);
-function callFun(){
-    // 撈出key資料
-    var str = localStorage.getItem('myName');
-    alert(str);
-}
+// 若要取得陣列裡的資料
+// 第三步：取得 localStorage 資料
+var getData = localStorage.getItem('countryItem');
+// 第四步：先將 string 轉為 array
+var countryArray = JSON.parse(getData);
 
-
-
-
+console.log(countryArray[0].farmer);

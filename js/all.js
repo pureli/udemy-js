d5191076fb8bbe3981731dc695641257b0ac971c
黎映君 <j89828@gmail.com>
2018-08-28 22:18:58 +0800

    練習 localStorage 存、取資料到瀏覽器資料庫

--- js/all.js
+++ js/all.js
@@ -1,12 +1,29 @@
-// 練習事件監聽 - 從父元素監聽子元素
-// 點li的東西，會顯示li裡面的文字內容
-var el = document.querySelector('.UL');
-el.addEventListener('click',show,false);
-function show(e){
-    // 因為只希望點擊li出現文字，若點擊ul則不出現
-    // reture 表示回傳空值，並且到此結束，不會執行接下來的程式碼
-    if(e.target.nodeName == 'UL'){return};
-    console.log(e.target.textContent);
-    // console.log(e.target.nodeName);
+// 練習 localStorage 存、取資料到瀏覽器資料庫
 
+// var str = 'hello';
+// localStorage.setItem("myName",str);
+// console.log(localStorage.getItem('myName'));
+
+// 練習撈文字
+// 第一步：click按鈕存資料到 local storage
+var btn = document.querySelector('.btnClass');
+
+
+btn.addEventListener('click',saveToStorage,false);
+function saveToStorage(){
+    // 存到 local storage
+    var text = document.querySelector('.textClass').value;
+    localStorage.setItem('myName',text);
 }
+// 第一步：新增一個按鈕，呼叫存到 local storage 裡的名字
+var btnCall = document.querySelector('.btnCall');
+btnCall.addEventListener('click',callFun,false);
+function callFun(){
+    // 撈出key資料
+    var str = localStorage.getItem('myName');
+    alert(str);
+}
+
+
+
+

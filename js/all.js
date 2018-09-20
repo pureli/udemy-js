af196169c37bb7ca9ff942b340258a53018d8d23
黎映君 <j89828@gmail.com>
2018-09-21 01:52:51 +0800

    練習 AJAX：使用者帳密輸入、回傳成功or失敗訊息

--- js/all.js
+++ js/all.js
@@ -1,55 +1,40 @@
-// 練習 AJAX
-// 第一步：建立一個 XMLHttpRequest
-// 第二步：傳送到對方伺服器要資料
-// 第三步：回傳資料到自己的瀏覽器
-// 第四步：拿到資料後再看要做什麼事
-// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// 任務：將使用者帳密存起來，送到伺服器存檔，並回傳 alert 註冊成功或失敗
+
+// 第一步：取得使用者輸入帳密
+var accountStr = document.querySelector('.account').value;
+var passwordStr = document.querySelector('.password').value;
+var send = document.querySelector('.send');
 
 
-// 第一步
-var data = new XMLHttpRequest();
+// 第二步：按鈕 click 送出 email、password 功能，傳送到伺服器，並回傳成功或失敗訊息
+function sendFunc(){
+    // 將帳號、密碼從成物件（JSON 格式）
+    var data = {};
+    data.email = accountStr;
+    data.password = passwordStr;
+    
+    // 傳到伺服器
+    var xhr = new XMLHttpRequest();
+    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+    xhr.setRequestHeader('Content-type','application/json');
+    // 送出資料前，先將資料轉成字串
+    var dataStr = JSON.stringify(data);
+    xhr.send(dataStr);
 
-// 第二步
-// readyState
-// 0 - 你已經產生一個 XMLHttpRequest，但是還沒有連接你要撈的資料
-// 1 - 你用了 open()，但你還沒有把資料傳過去
-// 2 - 偵測到你有用 send
-// 3 - loading
-// 4 - 你撈到資料了，數據已經完全接收到了
-
-
-// 格式、要讀取的網址、同步與非同步
-// 格式：get（讀取資料）、post（傳送資料到伺服器）
-// 同步與非同步：
-// true - 非同步，不會等資料傳回來，就讓城市繼續往下跑，等到回傳才會自動回傳
-// false - 他會等資料傳回來，才讓程式碼繼續往下跑
-
-data.open('get','https://hexschool.github.io/ajaxHomework/data.json',true);
-
-// 第三步
-// 傳空的資料
-data.send(null);
-
-
-// 第四部
-// 顯示資料
-data.onload = function(){
-    // status 可判斷資料有無撈到
-    // 200 資料有正確回傳，有撈到
-    // 404 資料讀取錯誤，沒撈到
-    if(data.status == 200){
-        console.log(data.responseText);
-        // 在網頁上顯示資料
-        // 第一步：先將字串轉成陣列
-        var str = JSON.parse(data.responseText);
-        // 第二步：顯示在網頁上
-        var show = document.querySelector('.show');
-        show.textContent = str[0].name;
-    }else{
-        console.log('資料錯誤');
+    // 當執行完上述，表示伺服器已撈到資料，即可進行接下來
+    xhr.onload = function(){
+        // 將回傳資料（字串）轉成物件格式
+        // responseText 為整個存起來的物件（含帳密）
+        var callBackData = JSON.parse(xhr.responseText);
+        if(callBackData.message == '帳號註冊成功'){
+            alert('恭喜你註冊成功');
+        }
+        if(callBackData.message == '此帳號已被使用'){
+            alert('此帳號已被使用');
+        }
+        
     }
+
 }
 
-// CORS 是否提供跨網域撈取資料
-// 例如政府的 opendata 不能跨網域撈資料（資安問題）
-// 要確認是否可跨網域撈，可到 test-cors.org 把 JSON 網址貼上，看狀態是否為 200
+send.addEventListener('click',sendFunc,false);
